5dad1ef928dd316f7581ebe1d69556485611deff
farmas07 <farmas07@localhost>
2013-01-02 16:30:37 +0100

    login

--- src/android/bde_forum/MainActivity.java
+++ src/android/bde_forum/MainActivity.java
@@ -9,14 +9,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
+		setContentView(R.layout.login);
 	}
 
 }
