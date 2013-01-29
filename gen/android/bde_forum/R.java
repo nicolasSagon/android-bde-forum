598005072228880deb981dd2eeb8eed40d72d624
Bastien <Bastien@localhost>
2013-01-29 19:58:25 +0100

    commit du projet apr?s affichage des posts

--- gen/android/bde_forum/R.java
+++ gen/android/bde_forum/R.java
@@ -12,6 +12,7 @@ public final class R {
     }
     public static final class color {
         public static final int background=0x7f040000;
+        public static final int black=0x7f040003;
         public static final int separation=0x7f040002;
         public static final int threads=0x7f040001;
     }
@@ -21,19 +22,33 @@ public final class R {
     }
     public static final class id {
         public static final int Login=0x7f080001;
+        public static final int auteur=0x7f080006;
+        public static final int auteur2=0x7f080008;
+        public static final int auteur3=0x7f08000a;
+        public static final int auteur4=0x7f08000c;
+        public static final int auteur5=0x7f08000e;
         public static final int connection=0x7f080004;
         public static final int gallery1=0x7f080000;
-        public static final int menu_settings=0x7f080009;
-        public static final int nouveau=0x7f080008;
-        public static final int page=0x7f080006;
-        public static final int page1=0x7f080005;
-        public static final int page2=0x7f080007;
+        public static final int menu_settings=0x7f080016;
+        public static final int nouveau=0x7f080015;
+        public static final int page=0x7f080013;
+        public static final int page1=0x7f080012;
+        public static final int page2=0x7f080014;
         public static final int password=0x7f080002;
+        public static final int post1=0x7f080007;
+        public static final int post2=0x7f080009;
+        public static final int post3=0x7f08000b;
+        public static final int post4=0x7f08000d;
+        public static final int post5=0x7f08000f;
+        public static final int repondre=0x7f080011;
+        public static final int reponse=0x7f080010;
         public static final int stayConnected=0x7f080003;
+        public static final int titre=0x7f080005;
     }
     public static final class layout {
         public static final int login=0x7f030000;
-        public static final int threads=0x7f030001;
+        public static final int posts=0x7f030001;
+        public static final int threads=0x7f030002;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;
@@ -41,6 +56,8 @@ public final class R {
     public static final class string {
         public static final int Sconnect=0x7f050006;
         public static final int app_name=0x7f050000;
+        public static final int auteur=0x7f050019;
+        public static final int avatar=0x7f050018;
         public static final int connect=0x7f050007;
         /**  Strings pour la page des threads 
          */
@@ -51,10 +68,17 @@ public final class R {
         public static final int menu_settings=0x7f050001;
         public static final int nouveau=0x7f050012;
         public static final int page=0x7f050009;
+        public static final int post1=0x7f050013;
+        public static final int post2=0x7f050014;
+        public static final int post3=0x7f050015;
+        public static final int post4=0x7f050016;
+        public static final int post5=0x7f050017;
         /**  Strings pour la page Login 
          */
         public static final int pseudo=0x7f050002;
         public static final int pseudo2=0x7f050003;
+        public static final int repondre=0x7f05001a;
+        public static final int reponse=0x7f05001b;
         public static final int thread1=0x7f05000b;
         public static final int thread2=0x7f05000c;
         public static final int thread3=0x7f05000d;
